3e0b49e5eb60813431ff6ee30ada72155f069dff
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-03-04 11:50:00 +0400

    adding classes for example of using conversion between different data representations in Java and in the DB

--- src/main/java/com/piuralsight/hibernatfundamentals/Main.java
+++ src/main/java/com/piuralsight/hibernatfundamentals/Main.java
@@ -1,32 +1,30 @@
 package com.piuralsight.hibernatfundamentals;
 
-import com.piuralsight.hibernatfundamentals.airport.OneWayTicket;
 
+import com.piuralsight.hibernatfundamentals.airport.Passenger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.time.LocalDate;
+
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernatefundamentals.m01.ex08");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernatefundamentals.m01.ex09");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
+        Passenger josh = new Passenger("Josh Smith");
+        josh.setVip(true);
 
+        Passenger mike = new Passenger("Michael Johonson");
+        mike.setVip(false);
 
-        OneWayTicket oneWayTicket = new OneWayTicket();
-        oneWayTicket.setNumber("DD7894");
-        oneWayTicket.setLastDepartureDate(LocalDate.of(2022,3,8));
-
-
-
-
-        em.persist(oneWayTicket);
-       
+        em.persist(josh);
+        em.persist(mike);
 
         em.getTransaction().commit();
+        em.close();
         emf.close();
 
     }
