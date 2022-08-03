0c5ad3e46af291e2bde736c6355b553dc99060ab
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-08-03 16:44:47 +0400

    add bike UI

---
+++ bike-ui/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ServiceService} from "./services/service.service";
+import {HttpClientModule} from "@angular/common/http";
+import { AdminComponent } from './components/admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [ServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
